abc8ad69b8f14ed852fd68935742931b130e0837
Jack Sloan <jbsloan10@gmail.com>
2021-04-19 14:50:18 -0500

    add github pages directory

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
+const static = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,24 +8,24 @@ module.exports = {
 	// for more information about preprocessors
 	preprocess: sveltePreprocess({
 		defaults: {
-			style: 'scss'
+			style: 'scss',
 		},
-		postcss: true
+		postcss: true,
 	}),
 
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		adapter: static({ out: 'docs' }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 
 		vite: {
 			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
-	}
+				noExternal: Object.keys(pkg.dependencies || {}),
+			},
+		},
+	},
 };
