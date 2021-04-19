e9a2d4293b8a874022f318d4cd59dac55bb3ff73
Jack Sloan <jbsloan10@gmail.com>
2021-04-19 11:28:42 -0500

    simplify draggable elements

--- src/lib/DragElement.svelte
+++ src/lib/DragElement.svelte
@@ -2,24 +2,14 @@
 	import { draggable } from '$lib/draggable';
 	import { createEventDispatcher } from 'svelte';
 	import { spring } from 'svelte/motion';
+	import { get } from 'svelte/store';
+	import { topIndex } from '$lib/store';
+	import { onMount } from 'svelte';
 
 	export let coords = spring({ x: 0, y: 0 });
-	export let zIndex;
-	export let props: any = {};
+	export let zIndex = 0;
 
 	const dispatch = createEventDispatcher();
-
-	function close() {
-		dispatch('close');
-	}
-
-	function minimize() {
-		dispatch('minimize');
-	}
-
-	function maximize() {
-		dispatch('maximize');
-	}
 
 	function handleDrag(event) {
 		coords.update(($coords) => ({
@@ -27,13 +17,23 @@
 			y: $coords.y + event.detail.dy,
 		}));
 	}
+
+	function updateZ() {
+		$topIndex++;
+		zIndex = get(topIndex);
+	}
+
+	onMount(() => {
+		updateZ();
+	});
 </script>
 
 <div
-	class="relative"
+	class="relative inline-block select-none cursor-move shadow-lg hover:shadow-xl transition-shadow duration-200"
+	use:draggable
+	on:dragstart={updateZ}
+	on:dragmove={handleDrag}
 	style="transform: translate({$coords.x}px, {$coords.y}px); z-index: {zIndex};"
 >
-	<div use:draggable on:dragstart on:dragmove={handleDrag} class="bg-gray-400">
-		<slot />
-	</div>
+	<slot />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,49 @@
 <script lang="ts">
 	import './app.scss';
-	import DragContainer from '$lib/DragContainer.svelte';
+	import DragElement from '$lib/DragElement.svelte';
+	import { spring } from 'svelte/motion';
+	import type { Spring } from 'svelte/motion';
+
+	const coords1 = createCoords();
+	const coords2 = createCoords();
+	const coords3 = createCoords();
+
+	function createCoords() {
+		return spring({ x: 0, y: 0 }, { stiffness: 0.1, damping: 0.3 });
+	}
+
+	function reset(c: Spring<{ x: number; y: number }>) {
+		c.update(() => ({ x: 0, y: 0 }));
+	}
+
+	function resetAllCoords() {
+		[coords1, coords2, coords3].forEach(reset);
+	}
 </script>
 
-<h1 class="px-2 bg-blue-500 text-white">Welcome to SvelteKit</h1>
-<p class="px-2 test">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<section class="flex flex-row py-1 bg-blue-500 text-white items-center">
+	<h1 class="px-2">Draggable Elements Example</h1>
+	<button
+		class="border border-blue-700 bg-white text-blue-700 px-2 rounded-md"
+		on:click={resetAllCoords}>Reset Position</button
+	>
+</section>
+<main class="w-screen h-screen p-2">
+	<DragElement coords={coords1}>
+		<div class="w-64 h-64 flex flex-row items-center justify-center bg-red-400">
+			<p>Drag Me</p>
+		</div>
+	</DragElement>
 
-<DragContainer />
+	<DragElement coords={coords2}>
+		<div class="w-64 h-64 flex flex-row items-center justify-center bg-green-400">
+			<p>Drag Me</p>
+		</div>
+	</DragElement>
 
-<style lang="scss">
-	.test {
-		@apply text-indigo-700;
-	}
-</style>
+	<DragElement coords={coords3}>
+		<div class="w-64 h-64 flex flex-row items-center justify-center bg-indigo-400">
+			<p>Drag Me</p>
+		</div>
+	</DragElement>
+</main>
