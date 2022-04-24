6981437512ff3b365a98943266fd6c04173f39f8
jacksloan <jbsloan10@gmail.com>
2022-04-24 11:25:38 -0500

    setup upgrade folder for gh-pages

--- upgrade/svelte.config.js
+++ upgrade/svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +14,10 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/svelte-draggable-element-example'
+		}
 	}
 };
 
