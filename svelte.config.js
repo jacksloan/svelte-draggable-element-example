2dfda74d84c3cb32fbb51d8e736af4c6bf0e332d
jacksloan <jbsloan10@gmail.com>
2022-04-24 12:48:06 -0500

    add github publish action

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		paths: {
 			base: dev ? '' : '/svelte-draggable-element-example'
 		}
